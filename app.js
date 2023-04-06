9e6f4e918da64e8a5c1a41f698f1cc9e06ffec61
Luis Henrique Ries <prof.ries@gmail.com>
2023-04-06 20:04:14 -0300

    Tarefa 5

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const rotaProduto = require('./rotas/produto_rotas')
 const app = express();
 const PORTA = 3000;
 
+const trataLog = (req, res, next) => {
+    console.log("[REQUEST]", `${req.method} ${req.originalUrl}`);
+    next();
+    console.log("[RESPONSE]", `${res.statusCode}`);
+}
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
@@ -15,6 +21,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/app_produtos')
     }).catch((error) => {
         console.log("Erro:", error)
     });
+
+app.use(trataLog);
 
 app.use('/api/produtos', rotaProduto);
 
